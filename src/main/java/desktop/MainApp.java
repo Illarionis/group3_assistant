109a0ba6fd831bcc220df8ad167af9b237b9c73c
Abdullah <116903524+regnumnumen@users.noreply.github.com>
2023-02-13 23:18:13 +0100

    Disabled the GUI for a few moments.

--- src/main/java/desktop/MainApp.java
+++ src/main/java/desktop/MainApp.java
@@ -1,16 +1,12 @@
 package desktop;
 
-import gui.DesktopLayout;
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) {
-        Scene s = new Scene(new DesktopLayout(), 1280, 960);
-        stage.setScene(s);
-        stage.show();
+
     }
 
     public static void main(String[] args) {
