96cf0b1d41355f77427b8f7818b8ffd61bb40598
Abdullah <116903524+regnumnumen@users.noreply.github.com>
2023-03-12 21:08:26 +0100

    Temporarily disabled the GUI.

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,14 +1,10 @@
-import engine.DigitalAssistant;
-import gui.UserSession;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) {
-        var assistant = new DigitalAssistant();
-        var session = new UserSession<String, String>();
-        session.start(stage, assistant);
+
     }
 
     public static void main(String[] args) {
