f7fee826e658ebb8521a350186f83b293014ff25
Abdullah <116903524+regnumnumen@users.noreply.github.com>
2023-03-18 20:22:20 +0100

    Disabled the GUI

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,18 +1,14 @@
-import core.SecuritySystem;
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public final class MainApp extends Application {
-    public static void main(String[] args) {
-        launch(args);
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage stage) {
+
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        final var security = new SecuritySystem(primaryStage);
-        final var startScene = new Scene(security, 300, 600);
-        primaryStage.setScene(startScene);
-        primaryStage.show();
+    public static void main(String[] args) {
+        launch(args);
     }
 }
