534a53df3a4dd69ae7deeea983d4a9ac87394358
Abdullah <116903524+regnumnumen@users.noreply.github.com>
2023-05-10 23:09:34 +0200

    Simplified how the chat works

--- src/main/java/gui/Chat.java
+++ src/main/java/gui/Chat.java
@@ -1,16 +1,34 @@
 package gui;
 
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 
 import java.util.List;
 
-public final class Chat extends AnchorPane {
+public final class Chat extends ScrollPane {
+    private final List<Node> messages;
 
+    /**
+     * Creates an empty chat.
+     **/
+    public Chat() {
+        final VBox scrollContent = new VBox();
+        messages = scrollContent.getChildren();
+
+        setContent(scrollContent);
+        setMinSize(300, 600);
+        setFitToHeight(true);
+        setFitToWidth(true);
+    }
+
+    /**
+     * Adds a new message to the chat.
+     *
+     * @param m The message that should be added to the chat.
+     **/
+    public void add(Message m) {
+        messages.add(m);
+    }
 }
