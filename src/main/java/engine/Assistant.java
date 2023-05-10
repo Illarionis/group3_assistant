8dd7c1391ab066417b75cfcf992b9d572ce1288e
Abdullah <116903524+regnumnumen@users.noreply.github.com>
2023-05-11 01:58:29 +0200

    Updated how the assistant works with the new skill interface.

--- src/main/java/engine/Assistant.java
+++ src/main/java/engine/Assistant.java
@@ -1,60 +1,60 @@
 package engine;
 
+import skill.Skill;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Map;
 
 public final class Assistant {
-    private static final String[] DEFAULT_REPLY = {
-            "Could not understand your message.",
-            "Did you perhaps forget to define the skill?",
-            "Or have you made a typing mistake?"
-    };
-    private final List<Skill> skillList = new ArrayList<>();
+    private final List<Skill> skills;
 
-    public void addSkill(Skill s) {
-        skillList.add(s);
+    public Assistant() {
+        skills = new ArrayList<>();
     }
 
-    public void removeSkill(Skill s) {
-        skillList.remove(s);
+    public void add(Skill s) {
+        skills.add(s);
     }
 
-    public String[] respond(String msg) {
-        for (Skill s : skillList) {
-            final Pattern p = Pattern.compile(s.getInput());
-            Matcher m = p.matcher(msg);
-            if (m.find()) {
-                System.out.println(m.group(0));
-                Boolean foundAll = false;
-                ArrayList<String> test = new ArrayList<>();
-                try {
-                    test.add(m.group(1)); //group 1 = first thing entre parenthese dans la phrase (first placeheolder)
-                }catch(Exception e){
-                    e.printStackTrace();
-                    System.out.println("-----------------------------ERROR----------------------------------------------------");
-                    return s.getOutput();
-                }
-                System.out.println(test.get(0));
-                int i = 2;
-                while(!foundAll){
-                    try {
-                        test.add(m.group(i)); //group 1 = first thing entre parenthese dans la phrase (first placeheolder)
-                        System.out.println(m.group(i));
-                        i++;
-                    }catch(Exception e){
-                        foundAll = true;
-                        String[] testArray = test.toArray(new String[0]);
-                        return s.getOutput(testArray);
-                    }
-                }
+    public void remove(Skill s) {
+        skills.remove(s);
+    }
 
-
-            }
-
-
+    public String respond(String s) {
+        /* *
+         * Attempting to find a direct match for the string.yy
+         * */
+        for (var skill : skills) {
+            final Map<String, String> associations = skill.getAssociations();
+            final String out = associations.get(s);
+            if (out != null) return out;
         }
-        return DEFAULT_REPLY;
+
+        /* *
+         * In case no direct matching was found, we could search all (input, output) associations
+         * to find the best matching input.
+         * */
+        for (var skill : skills) {
+            final Map<String, String> associations = skill.getAssociations();
+            for (String input : associations.keySet()) {
+                /* *
+                 * Attempt to find an input with at least 95% similarity
+                 * This implies in a 100 character string, 5 characters are allowed to be wrongly spelled or be missing
+                 * */
+                int count = Math.min(input.length(), s.length());
+                int matches = 0;
+                for (int i = 0; i < s.length(); i++) {
+                    if (s.charAt(i) == input.charAt(i)) matches++;
+                }
+                double percentage = 100.0 * matches / input.length();
+                if (percentage > 95.0)  return associations.get(input);
+            }
+        }
+
+        /* *
+         * In case nothing is found, we simply return the default message.
+         * */
+        return "Could not recognize your message.";
     }
 }
