94584c382fe15a890db4de0584457ac0b4a1e343
natagutie <natagutie02@gmail.com>
2023-05-11 12:26:37 +0200

    merging of first draft of the CYK algorithm

--- src/main/java/skill/GrammarSkill.java
+++ src/main/java/skill/GrammarSkill.java
@@ -67,12 +67,58 @@ public final class GrammarSkill implements Skill {
      * This check is done by performing the CYK-algorithm,
      *
      * */
-    private void validateInput(String input) {
-        /* *
-         * Todo: @Natalia implement the CYK algorithm here.
-         *       You may check the algorithm by completing the main function at the bottom of this file.
-         * */
-        throw new IllegalArgumentException("Input " + input + " does not belong to the grammar.");
+    private boolean validateInput(String input) {
+        int n = input.length();
+        if (n == 0) return false;
+        boolean[][][] table = new boolean[n][n][nonTerminals.size()];
+
+        // Fill the first row of the table
+        for (int i = 0; i < n; i++) {
+            for (String t : terminals) {
+                if (input.substring(i, i + 1).equals(t)) {
+                    for (int j = 0; j < nonTerminals.size(); j++) {
+                        String nt = nonTerminals.get(j);
+                        if (rules.containsKey(nt) && rules.get(nt).contains(new String[]{t})) {
+                            table[0][i][j] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        // Fill the rest of the table
+        for (int l = 2; l <= n; l++) {
+            for (int i = 0; i <= n - l; i++) {
+                for (int j = 1; j <= l - 1; j++) {
+                    for (int k = 0; k < nonTerminals.size(); k++) {
+                        String nt = nonTerminals.get(k);
+                        if (rules.containsKey(nt)) {
+                            List<String[]> alternatives = rules.get(nt);
+                            for (String[] a : alternatives) {
+                                String b = a[0];
+                                String c = a[1];
+                                int bIndex = nonTerminals.indexOf(b);
+                                int cIndex = nonTerminals.indexOf(c);
+                                if (bIndex >= 0 && cIndex >= 0 && table[j - 1][i][bIndex] && table[l - j - 1][i + j][cIndex]) {
+                                    table[l - 1][i][k] = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Check if the input belongs to the grammar
+        for (int i = 0; i < nonTerminals.size(); i++) {
+            if (rules.containsKey(startSymbol) && rules.get(startSymbol).contains(new String[]{nonTerminals.get(i)})
+                    && table[n - 1][0][i]) {
+                return true;
+            }
+        }
+        return false;
+
+    throw new IllegalArgumentException("Input " + input + " does not belong to the grammar.");
     }
 
     public void addRule(String leftSide, String... rightSide) {
