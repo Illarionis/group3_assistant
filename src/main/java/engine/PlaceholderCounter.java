d1950c03c0b705159bc8d2c30fbb9c4449714961
Abdullah <116903524+regnumnumen@users.noreply.github.com>
2023-05-11 04:40:07 +0200

    Defined a tool to count placeholders in a template string.

---
+++ src/main/java/engine/PlaceholderCounter.java
@@ -0,0 +1,26 @@
+package engine;
+
+public final class PlaceholderCounter {
+    /**
+     * Counts the number of placeholders present in a template string.
+     *
+     * @param template The string containing placeholders for which the placeholders should be counted.
+     * @param placeholder The placeholder character sequence used in the template string.
+     * @return An integer >= 0
+     **/
+    public static int countPlaceholders(String template, String placeholder) {
+        int count = 0;
+        int startIndex = 0;
+        while (startIndex < template.length()) {
+            final int endIndex = startIndex + placeholder.length();
+            final String s = template.substring(startIndex, endIndex);
+            if (s.equals(placeholder)) {
+                count++;
+                startIndex = endIndex;
+            } else {
+                startIndex++;
+            }
+        }
+        return count;
+    }
+}
