a48927aee15096be4627445250f71bded78a1b54
Abdullah <116903524+regnumnumen@users.noreply.github.com>
2023-05-11 02:56:45 +0200

    Added top center alignment for the display order of messages

--- src/main/java/gui/Chat.java
+++ src/main/java/gui/Chat.java
@@ -1,5 +1,7 @@
 package gui;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.*;
@@ -13,10 +15,13 @@ public final class Chat extends ScrollPane {
      * Creates an empty chat.
      **/
     public Chat() {
+        final Insets padding = new Insets(5, 5, 5, 5);
         final VBox scrollContent = new VBox();
+        scrollContent.setAlignment(Pos.TOP_CENTER);
         messages = scrollContent.getChildren();
 
         setContent(scrollContent);
+        setPadding(padding);
         setMinSize(300, 600);
         setFitToHeight(true);
         setFitToWidth(true);
