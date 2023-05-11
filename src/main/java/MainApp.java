f29c6149dad5a5f2b58914818129ab7b0fa8b01d
Abdullah <116903524+regnumnumen@users.noreply.github.com>
2023-05-11 05:36:32 +0200

    Enabled the chat window

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,9 +1,21 @@
+import engine.Assistant;
+import gui.Chat;
+import gui.ChatWindow;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public final class MainApp extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        throw new IllegalStateException("Not yet implemented!");
+    public void start(Stage primaryStage) {
+        final var assistant = new Assistant();
+        final var chat = new Chat();
+        final var chatWindow = new ChatWindow(assistant, chat);
+
+        final var scene = new Scene(chatWindow);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Digital Assistant");
+        primaryStage.show();
     }
 }
