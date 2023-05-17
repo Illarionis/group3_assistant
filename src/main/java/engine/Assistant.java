c5203ce0eb62c5373d91e9ff2842ec69b52251d1
Abdullah <116903524+regnumnumen@users.noreply.github.com>
2023-05-17 19:23:05 +0200

    Re-defined how the assistant works

--- src/main/java/engine/Assistant.java
+++ src/main/java/engine/Assistant.java
@@ -1,60 +1,47 @@
 package engine;
 
-import skill.Skill;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 public final class Assistant {
-    private final List<Skill> skills;
+    /**
+     * Provides a map to store user defined (input, output)-associations.
+     **/
+    private final Map<String, String> associations = new HashMap<>();
 
-    public Assistant() {
-        skills = new ArrayList<>();
+    /**
+     * Associates an input with an output.
+     *
+     * @param in  The input that should be associated with an output.
+     * @param out The output that should be associated with the input.
+     * @return The output that was previously associated with the input. <br>
+     *         Null, if no previous association was found.
+     **/
+    public String associate(String in, String out) {
+        if (in == null) throw new IllegalArgumentException("Null is not to be used allowed as input!");
+        else if (out == null) throw new IllegalArgumentException("Null is not allowed to be used as an output!");
+        return associations.put(in, out);
     }
 
-    public void add(Skill s) {
-        if (!skills.contains(s)) skills.add(s);
+    /**
+     * Gets the output associated with an input.
+     *
+     * @param in The input for which an association should be found.
+     * @return The output associated with the input. <br>
+     *         Null, if no association was found.
+     **/
+    public String getAssociation(String in) {
+        return associations.get(in);
     }
 
-    public void remove(Skill s) {
-        skills.remove(s);
-    }
-
-    public String respond(String s) {
-        /* *
-         * Attempting to find a direct match for the string.yy
-         * */
-        for (var skill : skills) {
-            final Map<String, String> associations = skill.getAssociations();
-            final String out = associations.get(s);
-            if (out != null) return out;
-        }
-
-        /* *
-         * In case no direct matching was found, we could search all (input, output) associations
-         * to find the best matching input.
-         * */
-        for (var skill : skills) {
-            final Map<String, String> associations = skill.getAssociations();
-            for (String input : associations.keySet()) {
-                /* *
-                 * Attempt to find an input with at least 95% similarity
-                 * This implies in a 100 character string, 5 characters are allowed to be wrongly spelled or be missing
-                 * */
-                int count = Math.min(input.length(), s.length());
-                int matches = 0;
-                for (int i = 0; i < count; i++) {
-                    if (s.charAt(i) == input.charAt(i)) matches++;
-                }
-                double percentage = 100.0 * matches / input.length();
-                if (percentage > 95.0)  return associations.get(input);
-            }
-        }
-
-        /* *
-         * In case nothing is found, we simply return the default message.
-         * */
-        return "Could not recognize your message.";
+    /**
+     * Removes an (input, output)-association from the assistant.
+     *
+     * @param in The input of (input, output)-association that should be removed.
+     * @return The output that was associated with the input. <br>
+     *         Null, if no associations was found.
+     **/
+    public String removeAssociation(String in) {
+        return associations.remove(in);
     }
 }
