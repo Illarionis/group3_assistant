f2f1f613d61ed4ac364914efa11cabcddc404192
Abdullah <116903524+regnumnumen@users.noreply.github.com>
2023-05-24 11:03:55 +0200

    Provided a simple test for the algorithm

---
+++ src/test/java/engine/CYKAlgorithmTest.java
@@ -0,0 +1,25 @@
+package engine;
+
+import org.junit.jupiter.api.Test;
+
+final class CYKAlgorithmTest {
+    @Test
+    public void testSolve() {
+        // Grammar example taken from : https://www.geeksforgeeks.org/cyk-algorithm-for-context-free-grammar/
+        ContextFreeGrammar g = new ContextFreeGrammar();
+        g.setStartSymbol("S");
+        g.registerNonTerminals("S", "A", "B", "C");
+        g.registerTerminals("a", "b");
+        g.createRule("S", "A", "B");
+        g.createRule("S", "B", "C");
+        g.createRule("A", "B", "A");
+        g.createRule("A", "a");
+        g.createRule("B", "C", "C");
+        g.createRule("B", "b");
+        g.createRule("C", "A", "B");
+        g.createRule("C", "a");
+
+        final String[] in = {"ba", "baaba"};
+        for (String s : in) assert CYKAlgorithm.solve(s, g);
+    }
+}
