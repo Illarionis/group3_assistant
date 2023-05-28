b8b215ffd81e8128ffbe9d9fa58bd612abd9762c
Abdullah <116903524+regnumnumen@users.noreply.github.com>
2023-05-28 19:40:49 +0200

    Added and commented Sasha's changes before overhaul

--- src/main/java/engine/faceDetection/FaceDetector.java
+++ src/main/java/engine/faceDetection/FaceDetector.java
@@ -41,3 +41,52 @@ public class FaceDetector {
         return absolutePath + "/" + targetDirectory + "haarcascade_frontalface_default.xml";
     }
 }
+//    Sasha's latest histogram changes
+//    public boolean faceDetected(String imageName){
+//        // Load OpenCV core lib
+//        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+//
+//        // Define the cascade classifier, based on the pretrained model, provided by OpenCV
+//        CascadeClassifier classifier = new CascadeClassifier(xmlFilePath());
+//
+//        // Histogram equalization
+//        String imageNameHistEq = histogramEqualization(imageName);
+//
+//        // Read image, save in Mat matrix
+//        String fullPathBasic = imageFilePath(imageName);
+//        String fullPathHist = imageFilePath(imageNameHistEq);
+//
+//        Mat inputBasic = Imgcodecs.imread(fullPathBasic);
+//        Mat inputHist = Imgcodecs.imread(fullPathHist);
+//
+//        // Detect faces, store in matrices
+//        MatOfRect faceDetectionsBasic = new MatOfRect();
+//        MatOfRect faceDetectionsHist = new MatOfRect();
+//
+//        classifier.detectMultiScale(inputBasic, faceDetectionsBasic);
+//        classifier.detectMultiScale(inputHist, faceDetectionsHist);
+//
+//        return (faceDetectionsBasic.toArray().length > 0) || (faceDetectionsHist.toArray().length > 0);
+//    }
+//
+//    private String histogramEqualization(String imageName){
+//        System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+//        String fullPath = imageFilePath(imageName);
+//
+//        Mat input = Imgcodecs.imread(fullPath, Imgcodecs.IMREAD_GRAYSCALE);
+//        Mat output = new Mat(input.rows(), input.cols(), input.type());
+//
+//        // 1. Split image into channels
+//        // 2. Equalize each
+//        // 3. Merge into a single Mat
+//        // 4. Save Mat as jpg
+//
+//        Imgproc.equalizeHist(input, output);
+//
+//        String newImageName = imageName + "_histogram_equalized";
+//
+//        String newFullPath = imageFilePath(newImageName);
+//        Imgcodecs.imwrite(newFullPath, output);
+//
+//        return newImageName;
+//    }
