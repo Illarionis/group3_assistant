af81e0d1fc9f2448f0c3baec861a33d0794f8a93
Illarionis <olexander.finkov@gmail.com>
2023-06-23 11:10:14 +0200

    Updated CamController: addNewFace()

--- src/main/java/engine/faceDetection/CamController.java
+++ src/main/java/engine/faceDetection/CamController.java
@@ -1,25 +1,45 @@
 package engine.faceDetection;
 
 import org.bytedeco.javacv.*;
+import org.bytedeco.opencv.opencv_core.IplImage;
+
+import static org.bytedeco.opencv.helper.opencv_imgcodecs.cvSaveImage;
 
 public class CamController {
-    private final FrameGrabber grabber;
+    private final FrameGrabber frameGrabber;
 
     public CamController() {
-        grabber = new OpenCVFrameGrabber(0);
+        frameGrabber = new OpenCVFrameGrabber(0);
     }
 
     public Frame takePicture(){
         try {
-            grabber.start();
+            frameGrabber.start();
 
             Thread.sleep(1000);
 
-            final var frame = grabber.grab();
+            final var frame = frameGrabber.grab();
 
-            grabber.close();
+            frameGrabber.close();
 
             return frame;
+
+        } catch (FrameGrabber.Exception | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void addNewFace(String personName){
+        OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
+        OpenCVFrameConverter.ToIplImage converter = new OpenCVFrameConverter.ToIplImage();
+        try {
+            grabber.start();
+            for(int i = 0; i < 5; i++){
+                IplImage image = converter.convert(grabber.grab());
+                cvSaveImage("face recognition data/" + personName + " (" + i + ")" + ".jpg", image);
+                Thread.sleep(50);
+            }
+            grabber.close();
 
         } catch (FrameGrabber.Exception | InterruptedException e) {
             throw new RuntimeException(e);
